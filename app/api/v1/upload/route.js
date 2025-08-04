e7ce89aca3deb9a7f1b5524e22a15493e5def256
vamshi <vsripathi@kenfront.com>
2025-08-04 17:38:51 +0530

    added cors

--- app/api/v1/upload/route.js
+++ app/api/v1/upload/route.js
@@ -1,11 +1,31 @@
+// import { put } from "@vercel/blob";
+// import { NextResponse } from "next/server";
+
+
+// export async function POST(request) {
+//     const {searchParams } = new URL(request.url);
+//     const filename = searchParams.get("filename");
+
+//     const blob = await put(filename,request.body,{access:"public",allowOverwrite: true})
+//     return NextResponse.json(blob);
+// }
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
+import { applyCORS } from "@/lib/cors";
 
+export async function OPTIONS() {
+  // Handle preflight CORS
+  return applyCORS(new NextResponse(null, { status: 204 }));
+}
 
 export async function POST(request) {
-    const {searchParams } = new URL(request.url);
-    const filename = searchParams.get("filename");
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get("filename");
 
-    const blob = await put(filename,request.body,{access:"public",allowOverwrite: true})
-    return NextResponse.json(blob);
+  const blob = await put(filename, request.body, {
+    access: "public",
+    allowOverwrite: true,
+  });
+
+  return applyCORS(NextResponse.json(blob));
 }
